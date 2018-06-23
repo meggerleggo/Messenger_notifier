d2d4b9c100abab09eaa1e838227872d963509a54
tiberiumdvk <dan6365282@gmail.com>
2018-06-23 21:31:06 +0200

    Add authentication.js

---
+++ src/authentication.js
@@ -0,0 +1,21 @@
+module.exports =
+{
+    isLoginRequired: async page =>
+    {
+        let cookies = await page.cookies();
+        return !cookies.some(value => value.name === 'c_user');
+    },
+
+    login: async (page, email, password) =>
+    {        
+        let emailBox = await page.waitForSelector('#email');
+        let passwordBox = await page.waitForSelector('#pass');
+        let loginButton = await page.waitForSelector('#loginbutton');
+    
+        await emailBox.type(email, {delay: 15});
+        await passwordBox.type(password, {delay: 15});
+        await loginButton.click({delay: 250});
+        
+        await page.waitForNavigation({waitUntil: "networkidle2"});
+    }
+}
